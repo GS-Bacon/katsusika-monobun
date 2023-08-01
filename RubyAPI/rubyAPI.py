fb267bf7c0489311b01cdf7e638060a11a0bd73f
bacon <bacon0817.ix@gmail.com>
2023-08-01 14:36:58 +0900

    ルビ振り追加 P2迄ルビ振り

--- RubyAPI/rubyAPI.py
+++ RubyAPI/rubyAPI.py
@@ -1,6 +1,9 @@
 import json
 from urllib import request
 import xml.etree.ElementTree as ET
+import regex #文字種判別
+from lxml import html
+import codecs
 
 APPID = "123"  # <-- ここにあなたのClient ID（アプリケーションID）を設定してください。
 URL = "https://jlp.yahooapis.jp/FuriganaService/V2/furigana"
@@ -26,13 +29,28 @@ def post(query):
         body = res.read()
     return body.decode()
 
+htmlfile=r'monogatari\Kunezumi\Aozora\kunezumi.html'
+with open(htmlfile,mode='rb') as f:
+    t=html.fromstring(f.read().decode('shiftJIS'))
+honbun=(t.text_content())
 
-response = post("漢字かな交じり文にふりがなを振ること。")
+p = regex.compile(r'\p{Script=Han}+') #漢字判定用正規表現らしい
+response = post(honbun)
+
+t=""
 #response_dumped=json.dumps(response)
 root= json.loads(response)
 result=root["result"]["word"]#ヘッダー切り離し
 for word in result:
-    if('furigana' in word):
-        print(len(word))
+    if('furigana' in word): #ふりがなが入っていたら furiganaが含まれる
+        if('subword'in word): #カナ交じりだったらsubwordが含まれる
+            for kanamajiri in word["subword"]:
+                if(p.fullmatch(kanamajiri["surface"])):
+                    t+=("\\ruby{"+kanamajiri["surface"]+"}"+"{"+kanamajiri["furigana"]+"}")
+                else:
+                    t+=(kanamajiri["surface"])
+        else:
+            t+=("\\ruby{"+word["surface"]+"}"+"{"+word["furigana"]+"}") #漢字のみのフリガナ設定
     else:
-        print ("a")
+        t+=str(word["surface"])
+print(t)
